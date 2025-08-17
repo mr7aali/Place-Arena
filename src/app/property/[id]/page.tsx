0bd105aa805f3be12812266ce38a69a8b7bfd66e
unknown <mr7aali@gmail.com>
2025-08-17 16:16:36 +0600

    solved error for dynamic page

--- src/app/property/[id]/page.tsx
+++ src/app/property/[id]/page.tsx
@@ -1,30 +1,10 @@
 import PropertyDetail from "./PropertyDetail";
 
-export async function generateStaticParams() {
-  return [
-    { id: "1" },
-    { id: "2" },
-    { id: "3" },
-    { id: "4" },
-    { id: "5" },
-    { id: "6" },
-    { id: "7" },
-    { id: "8" },
-    { id: "9" },
-    { id: "10" },
-    { id: "11" },
-    { id: "12" },
-  ];
-}
-// interface PropertyPageProps {
-//   params: { id: string };
-// }
 export default async function PropertyPage({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
-  // const params = useParams<{ id: string }>();
   const { id } = await params;
   console.log(id);
   const res = await fetch(
