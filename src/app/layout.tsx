147681d9c8480ed1633d2452458e73e3f8ade4e8
unknown <mr7aali@gmail.com>
2025-08-20 02:59:48 +0600

    added vercel.json file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono, Pacifico } from "next/font/google";
+import { Geist, Geist_Mono, Pacifico } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import dynamic from "next/dynamic";
 const Footer = dynamic(() => import("../components/Footer"));
-// const pacifico = Pacifico({
-//   weight: "400",
-//   subsets: ["latin"],
-//   display: "swap",
-//   variable: "--font-pacifico",
-// });
+const pacifico = Pacifico({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-pacifico",
+});
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Readdy Site",
@@ -34,8 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body
-        // className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable} antialiased`}
-        className="antialiased"
+        className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable} antialiased`}
       >
         <Header />
         {children}
